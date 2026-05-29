7dad8ad894170b4a03d2ad1f47162c53b8c18fde
Govinda Pandit <govinda.pandit@gmail.com>
2026-05-29 12:54:02 +0530

    testing phase

--- backend/server.js
+++ backend/server.js
@@ -126,22 +126,9 @@ app.get('/api/db-status', (req, res) => {
 
 
 
-const allowedOrigins = [
-    'http://localhost:5173', // Aapka local frontend
-    'https://health-care-drab-seven.vercel.app' // Aapka Vercel live frontend
-];
-
+// Test karne ke liye temporary sab hata kar sirf ye likh do:
 app.use(cors({
-    origin: function (origin, callback) {
-        // allow requests with no origin (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
-    credentials: true
+    origin: "*"
 }));
 
 const BASE_PORT = parseInt(process.env.PORT, 10) || 5000;
